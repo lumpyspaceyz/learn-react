aac5e0accd61f02b6feeee04e8ae416bb1df7106
lumpyspaceyz <yzever99@gmail.com>
2024-01-19 10:42:24 +0900

    [실습] TypeScript 브라우저 실시간 컴파일

---
+++ src/lecture/09-typescript-runtime.tsx
@@ -0,0 +1,20 @@
+// import React from 'https://esm.sh/react' -> TS가 허락하지 않음
+
+// @ts-ignore
+const {createRoot} = ReactDOM;
+
+const createApp = () => {
+  return (
+    <div id="app">
+      <h1>안녕!<br />리액트</h1>
+      <p>리액트는 오픈 소스 자바스크립트 라이브러리입니다</p>
+    </div>
+  )
+}
+
+const rootElememt = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(rootElememt)
+
+root.render(
+  createApp()
+)
