8f99e1b9352e9b853c9e2e84b8496f46e8bc8bf3
lumpyspaceyz <yzever99@gmail.com>
2024-01-24 14:41:29 +0900

    [원론] 리스트 렌더링 Part 4

---
+++ src/lecture/25-mapping-over-data-v0.3.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-key */
+
+import './25-mapping-over-data.css';
+import contactData from '../data/contacts.json';
+import { ContactCard, ContactCardList } from './23-contact-card';
+
+export default function Exercise() {
+  return (
+    <ContactCardList>
+      {contactData.items.map((item /* { name, job, ... } */) => (
+        // <ContactCard {...item} />
+        // ⏷
+        // <ContactCard {name, job, ...} />
+        // ⏷
+        // React.createElement(ContactCard, {name, job, ...})
+        <ContactCard {...item} />
+      ))}
+    </ContactCardList>
+  );
+}
