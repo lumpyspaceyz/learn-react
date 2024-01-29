4e9d737ab4facdfd6fc81530268c882a11253a4b
lumpyspaceyz <yzever99@gmail.com>
2024-01-29 14:52:42 +0900

    [준비] 상태 관리

---
+++ src/lecture/32-react-state-management.jsx
@@ -0,0 +1,13 @@
+import { CountUp } from '../components/CountUp'
+
+function Exercise() {
+  return (
+    <div>
+      <h2>Exercise 32</h2>
+      <CountUp />
+      <CountUp count={9} />
+    </div>
+  )
+}
+
+export default Exercise
