6846cb908dc0018c973b9f4eeaf9b267f8c75d29
lumpyspaceyz <yzever99@gmail.com>
2024-01-29 14:16:38 +0900

    [원론] 멀티 이벤트 핸들링

--- src/lecture/31-event-handling.jsx
+++ src/lecture/31-event-handling.jsx
@@ -1,54 +1,71 @@
 let count = 100;
-
 console.log(`count = ${count}`);
 
 function Exercise() {
+
   const handleClick = (e) => {
-    // Event 객체 (합성 이벤트)
-    // console.log(e);
-
-    // Event 객체 (브라우저 실제 이벤트)
-    // console.log(e.nativeEvent);
-
-    // 이벤트 전파 중지
-    e.stopPropagation();
-
-    console.log('-------------------------------');
-    console.log(`e.target = ${e.target.nodeName}`); // 클릭한 요소(이벤트가 전파된 요소)
-    console.log(`e.currentTarget = ${e.currentTarget.nodeName}`);
-  };
+    // console.log(e); // Event 객체(합성 이벤트)
+    // console.log(e.nativeEvent); // Event 객체 (브라우저의 실제 이벤트)
+    console.log(e.target);
+    // console.log(e.currentTarget);
+  }
 
   const handleCountUp = (e) => {
     e.stopPropagation();
     console.log(`count = ${++count}`);
-  };
+  }
 
-  return (
-    <div lang="en" style={styles.div} onClick={handleClick}>
-      <strong style={styles.strong} onClick={handleClick}>
-        strong element
-      </strong>{' '}
-      <em style={styles.em} onClick={handleClick}>
-        emphasize element
-      </em>
-      <button
-        type="button"
-        // inline arrow function
-        onClick={
-          /* handleCountDown */
-          (e) => {
-            e.stopPropagation();
-            console.log(`count = ${--count}`);
-          }
-        }
-      >
-        count down
-      </button>
-      <button type="button" onClick={handleCountUp}>
-        count up
-      </button>
-    </div>
-  );
+  const handleCountDown = (e) => {
+    e.stopPropagation();
+    console.log(`count = ${--count}`);
+  }
+
+  const actionOne = () => console.log('one');
+  const actionTwo = () => console.log('two');
+  const actionThree = () => console.log('three');
+
+  const handleMultiEvents = (e) => {
+    e.stopPropagation();
+    actionOne()
+    actionTwo()
+    actionThree()
+  }
+
+  return <div
+    style={styles.div}
+    onClick={handleClick}
+  >
+    <strong
+      style={styles.strong}
+      onClick={handleClick}
+    >
+      Strong element
+    </strong>
+    <em
+      style={styles.em}
+      onClick={handleClick}
+    >
+      emphasize element
+    </em>
+    <button
+      type="button"
+      onClick={handleCountDown}
+    >
+      count down
+    </button>
+    <button
+      type="button"
+      onClick={handleCountUp}
+    >
+      count up
+    </button>
+    <button
+      type="button"
+      onClick={handleMultiEvents}
+    >
+      multi event handling
+    </button>
+  </div>
 }
 
 const styles = {
@@ -65,7 +82,7 @@ const styles = {
   em: {
     padding: 20,
     border: '1px solid green',
-  },
-};
+  }
+}
 
 export default Exercise;
